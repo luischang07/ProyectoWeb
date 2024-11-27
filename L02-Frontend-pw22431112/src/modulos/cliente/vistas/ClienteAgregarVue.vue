b9f31e4be6370032469fc4353135c4832c628ecb
Luis Lopez <l20170728@culiacan.tecnm.mx>
2024-11-27 16:53:40 -0700

    Validaciones agregadas

--- L02-Frontend-pw22431112/src/modulos/cliente/vistas/ClienteAgregarVue.vue
+++ L02-Frontend-pw22431112/src/modulos/cliente/vistas/ClienteAgregarVue.vue
@@ -3,6 +3,8 @@ import { ref, watch } from 'vue';
 import type { ClienteAgregar } from '../interfaces/cliente-interface';
 import { useCliente } from '../controladores/useCliente';
 import { errorToast, sucessToast } from '@/modulos/utils/displayToast';
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import { clienteSchema } from '../schemas/ClienteSchema';
 
 const { setCliente, mensaje } = useCliente();
 
@@ -13,6 +15,10 @@ let cliente = ref<ClienteAgregar>({
     correo_electronico: '',
     ciudad: ''
 });
+
+const onValidated = async () => {
+    await setCliente(cliente.value);
+};
 
 const showErrorToast = () => {
     if (mensaje.value && mensaje.value[0] !== 'Cliente agregado con éxito') {
@@ -33,13 +39,13 @@ watch(mensaje, () => {
 
 watch(mensaje, () => {
     if (mensaje.value && mensaje.value[0] === 'Cliente agregado con éxito') {
-        // cliente.value = {
-        //     nombre: '',
-        //     direccion: '',
-        //     telefono: '',
-        //     correo_electronico: '',
-        //     ciudad: ''
-        // };
+        cliente.value = {
+            nombre: '',
+            direccion: '',
+            telefono: '',
+            correo_electronico: '',
+            ciudad: ''
+        };
     }
 });
 </script>
@@ -57,36 +63,78 @@ watch(mensaje, () => {
                 {{ error }}
             </div>
             <div class="card-body">
-                <div class="mb-3">
-                    Nombre
-                    <input type="text" class="form-control" v-model="cliente.nombre" placeholder="Nombre">
-                </div>
-                <div class="mb-3">
-                    Dirección
-                    <input type="text" class="form-control" v-model="cliente.direccion" placeholder="Dirección">
-                </div>
-                <div class="mb-3">
-                    Teléfono
-                    <input type="text" class="form-control" v-model="cliente.telefono" placeholder="Teléfono">
-                </div>
-                <div class="mb-3">
-                    Correo Electrónico
-                    <input type="email" class="form-control" v-model="cliente.correo_electronico" placeholder="Correo Electrónico">
-                </div>
-                <div class="mb-3">
-                    Ciudad
-                    <input type="text" class="form-control" v-model="cliente.ciudad" placeholder="Ciudad">
-                </div>
-                <div class="mb-3">
-                    <button class="btn btn-primary" @click="setCliente(cliente)">Agregar</button>
-                </div>
+                <Form :validation-schema="clienteSchema" @submit="onValidated">
+                    <div class="mb-3">
+                        Nombre
+                        <Field 
+                            type="text" 
+                            class="form-control" 
+                            name="nombre" 
+                            v-model="cliente.nombre" 
+                            placeholder="Nombre"
+                        />
+                        <ErrorMessage name="nombre" class="errorValidacion" />
+                    </div>
+                    <div class="mb-3">
+                        Dirección
+                        <Field 
+                            type="text" 
+                            class="form-control" 
+                            name="direccion" 
+                            v-model="cliente.direccion" 
+                            placeholder="Dirección"
+                        />
+                        <ErrorMessage name="direccion" class="errorValidacion" />
+                    </div>
+                    <div class="mb-3">
+                        Teléfono
+                        <Field 
+                            type="text" 
+                            class="form-control" 
+                            name="telefono" 
+                            v-model="cliente.telefono" 
+                            placeholder="Teléfono"
+                        />
+                        <ErrorMessage name="telefono" class="errorValidacion" />
+                    </div>
+                    <div class="mb-3">
+                        Correo Electrónico
+                        <Field 
+                            type="email" 
+                            class="form-control" 
+                            name="correo_electronico" 
+                            v-model="cliente.correo_electronico" 
+                            placeholder="Correo Electrónico"
+                        />
+                        <ErrorMessage name="correo_electronico" class="errorValidacion" />
+                    </div>
+                    <div class="mb-3">
+                        Ciudad
+                        <Field 
+                            type="text" 
+                            class="form-control" 
+                            name="ciudad" 
+                            v-model="cliente.ciudad" 
+                            placeholder="Ciudad"
+                        />
+                        <ErrorMessage name="ciudad" class="errorValidacion" />
+                    </div>
+                    <div class="mb-3">
+                        <button type="submit" class="btn btn-primary">Agregar</button>
+                    </div>
+                </Form>
             </div>
         </div>    
     </div>
 </template>
 
+
 <style scoped>
 .table-hover tr:hover {
     background-color: #f5f5f5;
 }
+.errorValidacion{
+        color: red;
+        font-weight: bold;
+    }
 </style>
