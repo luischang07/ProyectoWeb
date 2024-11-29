467db72b9d679cae4fc2c60a9b2c02177cb178d3
Luis Lopez <l20170728@culiacan.tecnm.mx>
2024-11-29 12:18:26 -0700

    Fix: Se arreglo el formato de las fechas y las validaciones del precio en el schema

--- APITypescript/src/schemas/articulo.Schema.ts
+++ APITypescript/src/schemas/articulo.Schema.ts
@@ -7,20 +7,21 @@ const validaciones = {
     descripcionMax: "La descripción debe tener menos de 300 caracteres",
     cantidad_en_almacen:"La cantidad en almacen debe ser un número entero",
     fecha_caducidad: "La fecha de caducidad debe tener el formato dd-mm-aaaa",
+    precio: "El precio debe ser un número positivo"
 };
 
 //validar datos con zod - construir schema
 export const ArticuloNuevoSchema = z.object({
     id: z.number().int(),
     descripcion: z.string().min(2,validaciones.descripcionMin).max(300,validaciones.descripcionMax),
-    precio: z.number(),
-    cantidad_en_almacen: z.number().int(),
+    precio: z.preprocess((val) => parseFloat(val as string), z.number().positive(validaciones.precio)),
+    cantidad_en_almacen: z.number().int().positive(validaciones.cantidad_en_almacen),
     fecha_caducidad: z.date()
 })
 
 export const ArticuloSchema = z.object({
     descripcion: z.string().min(2,validaciones.descripcionMin).max(300,validaciones.descripcionMax),
-    precio: z.number(),
-    cantidad_en_almacen: z.number().int(),
+    precio: z.preprocess((val) => parseFloat(val as string), z.number().positive(validaciones.precio)),
+    cantidad_en_almacen: z.number().int().positive(validaciones.cantidad_en_almacen),
     fecha_caducidad: z.date()
 })
